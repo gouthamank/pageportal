1fa1b5a5639b45d831327e4d6fde53efea1bad5a
gouthamank <gouthaman.k@gmail.com>
2022-02-25 21:20:13 +0400

    Colors addition

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,13 @@ interface ContainerProps {
 }
 
 const SectionContainer = styled.div<ContainerProps>`
-  height: 100vh;
+  min-height: 100vh;
   background-color: ${(props) => props.color};
   margin-bottom: 50px;
   position: relative;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 const SampleTextLeft = styled.span`
@@ -22,6 +22,10 @@ const SampleTextLeft = styled.span`
   font-size: 24px;
   color: ${(props) => props.theme.colors.contentHard};
   letter-spacing: -1px;
+`;
+
+const TextParagraph = styled.p`
+  max-width: 900px;
 `;
 
 export default function RootPage() {
@@ -33,11 +37,54 @@ export default function RootPage() {
       </Head>
       <SectionContainer color={theme.colors.backgroundSoft}>
         <SampleTextLeft>
-          Autem omnis alias doloribus odit commodi. Voluptas deserunt ipsam
-          quisquam quaerat ut quis aliquid. Aut architecto et temporibus et
-          adipisci quibusdam. Ullam harum est facere modi et. Ea mollitia
-          dolorem quia aut assumenda. Minima dolore ex excepturi recusandae nisi
-          vero.
+          <TextParagraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Consectetur purus ut faucibus pulvinar. Suscipit tellus mauris a
+            diam maecenas sed enim ut. Eget velit aliquet sagittis id. In
+            aliquam sem fringilla ut morbi tincidunt augue interdum. Quam
+            viverra orci sagittis eu. Neque aliquam vestibulum morbi blandit
+            cursus risus at ultrices. Tellus in hac habitasse platea dictumst
+            vestibulum rhoncus est. Quam pellentesque nec nam aliquam sem.
+            Volutpat maecenas volutpat blandit aliquam etiam erat velit
+            scelerisque. Leo urna molestie at elementum eu facilisis sed odio.
+            Viverra maecenas accumsan lacus vel facilisis. Volutpat maecenas
+            volutpat blandit aliquam etiam erat. Netus et malesuada fames ac
+            turpis egestas integer. Velit euismod in pellentesque massa. Ut
+            aliquam purus sit amet luctus venenatis lectus magna fringilla.
+            Facilisi cras fermentum odio eu.
+          </TextParagraph>
+          <TextParagraph>
+            Cras semper auctor neque vitae. Blandit cursus risus at ultrices mi
+            tempus imperdiet nulla. Mattis nunc sed blandit libero volutpat sed
+            cras ornare arcu. Laoreet suspendisse interdum consectetur libero id
+            faucibus nisl. Volutpat lacus laoreet non curabitur gravida arcu ac
+            tortor. Pharetra diam sit amet nisl suscipit adipiscing bibendum
+            est. Ut sem viverra aliquet eget sit amet tellus cras. Commodo quis
+            imperdiet massa tincidunt nunc pulvinar sapien et ligula. Pharetra
+            sit amet aliquam id diam maecenas ultricies mi eget. Faucibus
+            interdum posuere lorem ipsum dolor sit amet consectetur. Blandit
+            turpis cursus in hac habitasse platea dictumst quisque sagittis. Non
+            curabitur gravida arcu ac tortor dignissim convallis aenean.
+            Ultricies leo integer malesuada nunc vel risus commodo viverra
+            maecenas. Justo donec enim diam vulputate ut pharetra. Molestie ac
+            feugiat sed lectus vestibulum mattis.
+          </TextParagraph>
+          <TextParagraph>
+            Adipiscing diam donec adipiscing tristique risus nec feugiat.
+            Egestas dui id ornare arcu odio ut sem nulla pharetra. Turpis
+            egestas pretium aenean pharetra magna ac placerat. Nec feugiat nisl
+            pretium fusce id velit ut. Neque aliquam vestibulum morbi blandit
+            cursus. Non nisi est sit amet. Dignissim diam quis enim lobortis
+            scelerisque. Nascetur ridiculus mus mauris vitae ultricies leo.
+            Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Commodo
+            sed egestas egestas fringilla phasellus. Accumsan in nisl nisi
+            scelerisque eu ultrices vitae. Integer eget aliquet nibh praesent.
+            Amet aliquam id diam maecenas ultricies mi eget mauris pharetra.
+            Pellentesque pulvinar pellentesque habitant morbi. Auctor elit sed
+            vulputate mi sit amet. Consectetur adipiscing elit ut aliquam purus
+            sit amet luctus.
+          </TextParagraph>
         </SampleTextLeft>
       </SectionContainer>
       <SectionContainer color={theme.colors.backgroundSoft} />
