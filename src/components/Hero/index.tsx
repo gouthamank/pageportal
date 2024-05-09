8215985511551104527235d1f77b1dc1b54379d3
gouthamank <gouthaman.k@gmail.com>
2024-05-09 01:35:34 -0400

    update: add theme switch

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import { Chivo } from 'next/font/google';
 import type { HeroProps } from '@/types/components/Hero/index.types';
 import className from 'classnames';
 import Image from 'next/image';
 import profileImage from '@/images/profile.jpg';
+import ThemeSwitch from '@/components/ThemeSwitch';
 
 const chivo = Chivo({ subsets: ['latin'] });
 
@@ -11,10 +14,13 @@ export default function Hero(props: HeroProps) {
         <div
             className={className([
                 chivo.className,
-                'sticky left-0 right-0 top-0 z-40 flex min-h-[20rem] flex-col items-center justify-center px-4 transition-colors md:px-6',
+                'sticky left-0 right-0 top-0 z-40 flex min-h-[20rem] flex-col items-center justify-center px-4 pb-6 pt-3 transition-colors md:px-6',
                 'bg-early-dawn-400 text-white dark:bg-early-dawn-800',
             ])}
         >
+            <div className='flex w-full flex-row justify-end'>
+                <ThemeSwitch />
+            </div>
             <div>
                 <p>Hi, I&apos;m</p>
                 <p className='text-xl'>Gouthaman Kumarappan.</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
         './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
     ],
+    darkMode: 'selector',
     theme: {
         screens: {
             sm: '480px',
